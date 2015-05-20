aa370eb3edb01dc8cc38b3183eeff088b8120138
Jonas Möller <jonas.moeller@web.de>
2015-05-20 15:16:53 +0200

    added Ui and Ui-control to end a quiz and restart the quiz

--- js/ui-control.js
+++ js/ui-control.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
     quizUi = new QuizUi();
     quizUi.question = "Das ist die erste Frage";
     quizUi.questionNo = "1";
+    quizUi.state = 1;
     quizUi.answers = new Array("tolle Antwort", "zweite tolle Antwort", "koennte falsch sein");
 });
 
@@ -13,29 +14,84 @@ $(document).on('click', '.answer-container', function (e) {
     quizUi.switchQuestion();
 });
 
+$(document).on('click', '#restart-quiz', function (e) {
+    quizUi.resetQuiz();
+});
+
 QuizUi = function() {
     var that = this;
 
+    var state;
     var question;
     var answers = new Array();
     var questionNo;
 
     this.startQuiz = function() {
-        this.appendQuestion();
-        this.appendAnswers();
+        if(this.state == 1) {
+            this.appendQuestion();
+            this.appendAnswers();
 
-        $('#start').fadeOut(500, function(){
-            $('#game').fadeIn(500);
-        });
+            $('#start').fadeOut(500, function(){
+                $('#game').fadeIn(500);
+            });
+
+            this.state = 2;
+        } else {
+            console.log("State is not correct for this action!");
+        }
 
     }
 
+    this.resetQuiz = function() {
+        if(this.state == 3) {
+            $('#result').fadeOut(500, function(){
+                $('#start').fadeIn(500);
+            });
+            this.state = 1;
+        } else {
+            console.log("State is not correct for this action!");
+        }
+    }
+
     this.switchQuestion = function() {
-        $('#game').fadeOut(500, function() {
-            that.appendQuestion();
-            that.appendAnswers();
-            $('#game').fadeIn(500);
-        });
+        if(this.state == 2) {
+            $('#game').fadeOut(500, function() {
+                that.appendQuestion();
+                that.appendAnswers();
+                $('#game').fadeIn(500);
+            });
+        } else {
+            console.log("State is not correct for this action!");
+        }
+    }
+
+    this.endQuiz = function(mood, correct, max) {
+        if(this.state == 2) {
+            $('#result-container').empty();
+
+            switch(mood) {
+                case 1:
+                    $('#result-container').append('<div class="alert alert-success"><h3><strong>Sehr gut!</strong></h3><p>Du hast <strong>' + correct + '</strong> von <strong>' + max + '</strong> Fragen richtig beantwortet!</p></div>');
+                    break;
+                case 2:
+                    $('#result-container').append('<div class="alert alert-warning"><h3><strong>Ganz Ok!</strong></h3><p>Du hast <strong>' + correct + '</strong> von <strong>' + max + '</strong> Fragen richtig beantwortet!</p></div>');
+                    break;
+                case 3:
+                    $('#result-container').append('<div class="alert alert-danger"><h3><strong>Das war nicht ganz so gut!</strong></h3><p>Du hast <strong>' + correct + '</strong> von <strong>' + max + '</strong> Fragen richtig beantwortet!</p></div>');
+                    break;
+                default:
+                    break;
+            }
+
+            $('#game').fadeOut(500, function() {
+                $('#result').fadeIn(500);
+            });
+
+            this.state = 3;
+        } else {
+            console.log("State is not correct for this action!");
+        }
+
     }
 
     this.clearAnswerBar = function() {
@@ -54,8 +110,8 @@ QuizUi = function() {
         this.question = input;
     }
 
-    this.setAnswers = function(input) {
-        this.answers = input;
+    this.setAnswers = function(arrayInput) {
+        this.answers = arrayIinput;
     }
 
     this.setQuestionNo = function(questionNo) {
@@ -81,7 +137,7 @@ QuizUi = function() {
         $('#answers').append(answersHtmlString);
 
         $('.answer-container').click(function(){
-            this.switchQuestion();
+            that.switchQuestion();
         });
 
     }
