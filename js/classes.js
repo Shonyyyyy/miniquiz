ff0bc13fa1a0f1d222d3eb5dd01d0f0f1dc45596
Björn Urban <bjoern.urban@gmx.de>
2015-05-21 13:10:33 +0200

    Parser hinzugefügt

--- js/classes.js
+++ js/classes.js
@@ -6,14 +6,79 @@
  * @constructor
  */
 Quiz = function() {
-    var questions = new Array(); //Questionobjects only!
+    var questions; //Questionobjects only!
     var maxRound = 6;
     var currentRound = 0;
-    var correctAnswers = new Array(); //Questionobjects only!
-
+    var correctAnswers; //Questionobjects only!
+	var that = this;
     this.addCorrectAnswer = function() {
 
+	
     }
+    this.init = function(){
+		this.readXml();
+	}
+	this.readXml = function(){
+       
+       var xmlhttp;
+       if(window.XMLHttpRequest){
+           xmlhttp = new XMLHttpRequest();
+       }
+       else{
+           xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+       }
+       xmlhttp.onreadystatechange = function(){
+           if(xmlhttp.readyState == 4)
+           {
+            	xmlText = xmlhttp.responseText;    //here we get all lines from text file*
+				that.parse(xmlText);
+           }
+       }
+       
+       xmlhttp.open("GET", "../Documents/Studium/miniquiz/quiz.xml", true);
+       xmlhttp.send();
+	}
+	this.parse = function(xmlText){
+		if (window.DOMParser)
+		{
+			parser=new DOMParser();
+			xmlDoc=parser.parseFromString(xmlText,"text/xml");
+		}
+		else // Internet Explorer
+		{
+			xmlDoc=new ActiveXObject("Microsoft.XMLDOM");
+			xmlDoc.async=false;
+			xmlDoc.loadXML(txt); 
+		}
+		var answerArray = new Array();
+		var question = new Array();
+		for(var c = 0; c < xmlDoc.getElementsByTagName("Question").length;c++){
+			
+			question[c] = new Question();
+			question[c].answers = new Array();
+			question[c].text = xmlDoc.getElementsByTagName("Question")[c].childNodes[1].childNodes[0].nodeValue;
+			question[c].id = xmlDoc.getElementsByTagName("Question")[c].getAttribute("id");
+			answer = new Answer();
+			answer.text=xmlDoc.getElementsByTagName("Question")[c].childNodes[3].childNodes[0].nodeValue;
+			answer.qId = question[c].id;
+			answer.correct = true;
+			
+			question[c].answers.push(answer);
+			ci = 5;
+			
+			for(i=0;i<xmlDoc.getElementsByTagName("Question")[c].childElementCount-2;i++){
+				answer = new Answer();
+				answer.text=xmlDoc.getElementsByTagName("Question")[c].childNodes[ci].childNodes[0].nodeValue;
+				answer.qId = question[c].id;
+				answer.correct = false;
+				question[c].answers.push(answer);
+				ci = ci+2;
+			}
+			
+		}
+		this.questions = question;
+		
+	}
 }
 
 /**
@@ -23,9 +88,9 @@ Quiz = function() {
  * @constructor
  */
 Question = function() {
-    var text = "";
+    var text;
     var id;
-    var answers = new Array();
+    var answers;
 }
 
 /**
@@ -35,9 +100,10 @@ Question = function() {
  * @constructor
  */
 Answer = function() {
-    var text = "";
+    var text;
     var qId;
     var correct;
+    
 }
 
 
---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    quizUi = new QuizUi();
+    quiz = new Quiz();
+    quiz.init();
+    
+    
+    /*quizUi.question = "Warum hat Janneck begonnen Angewandte Informatik zu studieren?";
+    quizUi.questionNo= "1";
+    quizUi.state = 1;
+    quizUi.answers = new Array("weil er ein krasser geek ist", "weil er seine Ausbildung geil fand", "weil er seine Ausbildung nicht so geil fand");*/
+});
